0047162b16af83d1eec79b11866efcd758e7659f
Nicolas Salas Berrios <nicolas.salas@ext.blaze.me>
2023-05-12 01:27:51 -0500

    First commit

---
+++ src/main/java/com/example/ubikatetracking/UbikateTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ubikatetracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbikateTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbikateTrackingApplication.class, args);
+    }
+
+}
